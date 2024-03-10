49a878b26aa24de8b6c01ac6750410d5a42ea656
Luke Packard <luke.packard@kellogg.northwestern.edu>
2024-03-10 21:11:30 +0000

    Getting even closer

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -24,9 +24,9 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
     @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    redirect_to "/places/#{@entry["user_id"]}"
   else
     flash["notice"] = "Login first."
   end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,11 +5,11 @@ class PlacesController < ApplicationController
   end
 
    
-    
-
+  
   def show
+    @user = User.find_by({ "id" => params["id"] })
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"]})
   end
 
   def new
